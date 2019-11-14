8aae60de9e05eb57a04c24c7885c63314de154f3
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-14 19:42:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array1 = []
-  array2 = string.split("")
-  string.each do |s|
-  s.to_a.reverse 
+  array2 = string.split(" ")
+  array2.each do |s|
+  s.reverse  
   end 
 end 
