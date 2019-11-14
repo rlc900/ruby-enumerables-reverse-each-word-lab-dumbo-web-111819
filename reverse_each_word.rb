a35a1287adcc1548f4399e213c6caf8f8f60c745
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-14 19:43:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end 
   array1.join(" ")
 end 
+
+def reverse_each_word(string)
+  array1 = []
+  array2 = string.split(" ")
+  array2.collect do |s|
+  array1 << s.reverse  
+  end 
+  array1.join(" ")
+end 
